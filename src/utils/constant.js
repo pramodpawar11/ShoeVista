3940422ab725f6ebaad29a6808c99db546abe6b7
pramodpawar11 <kingpramodpawar511@gmail.com>
2024-02-12 09:32:13 +0530

    building footer and review section

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -90,12 +90,12 @@ export const footerLinks = [
     {
         title: "Products",
         links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
+            { name: "Force 1", link: "/" },
+            { name: "Max 1", link: "/" },
+            { name: "Jordan 1", link: "/" },
+            { name: "Force 2", link: "/" },
+            { name: "Vista Waffle Racer", link: "/" },
+            { name: "Vista Cortez", link: "/" },
         ],
     },
     {
@@ -111,7 +111,7 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "customer@vista.com", link: "mailto:customer@vista.com" },
             { name: "+92554862354", link: "tel:+92554862354" },
         ],
     },
